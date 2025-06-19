393e38af722c10543bba9ff1a362c4930fa790cb
unknown <sankagunarathna3@gmail.com>
2025-06-19 17:42:32 +0530

    Update styles and add TopDoctors component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
+import Specialitymenu from '../components/specialitymenu'
+import TopDoctors from '../components/TopDoctors'
 
 function Home() {
   return (
     <div>
       <Header/>
+      <Specialitymenu/>
+      <TopDoctors/>
     </div>
   )
 }
